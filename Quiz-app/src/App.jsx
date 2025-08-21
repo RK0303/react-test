473de5e2a6b40c17a6a87bda94fbd6ab71f6a911
RK0303 <ruthish3@gmail.com>
2025-08-21 08:22:46 +0530

    updated

--- Quiz-app/src/App.jsx
+++ Quiz-app/src/App.jsx
@@ -1,12 +1,12 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import QuestionsData from "./questions.json";
 
 import "./index.css";
 import Button from "./components/button/ButtonOption";
 import Option from "./container/options/option";
 import Timer from "./container/timer/Timer";
-import Question from "./components/question-section/Question-section";
-import Score from "./components/score-section/Score";
+import Question from "./container/question-section/Question-section";
+import Score from "./container/score-section/Score";
 
 const App = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -16,42 +16,42 @@ const App = () => {
 
   useEffect(() => {
     let interval;
-    if(timer > 0 && !showScore){
-      interval = setInterval(()=>{
-        setTimer((preTimer) => preTimer -1);
-      }, 1000)
-    }else{
+    if (timer > 0 && !showScore) {
+      interval = setInterval(() => {
+        setTimer((preTimer) => preTimer - 1);
+      }, 1000);
+    } else {
       clearInterval(interval);
       setShowScore(true);
     }
 
-    return () => clearInterval(interval)
+    return () => clearInterval(interval);
   }, [timer, showScore]);
-  
 
   return (
     <main className="flex justify-center items-center w-screen h-screen bg-blue-200">
       <div className="quiz-app rounded-2xl w-2xl bg-blue-50 p-4 text-center">
-        {
-          showScore ? <Score score={score} 
-                      setCurrentQuestion={setCurrentQuestion}
-                      setScore={setScore}
-                      setShowScore={setShowScore}
-                      setTimer={setTimer}
-                      /> :
+        {showScore ? (
+          <Score
+            score={score}
+            setCurrentQuestion={setCurrentQuestion}
+            setScore={setScore}
+            setShowScore={setShowScore}
+            setTimer={setTimer}
+          />
+        ) : (
           <>
             <Question currentQuestion={currentQuestion} />
-            <Option currentQuestion={currentQuestion} 
-                    setCurrentQuestion={setCurrentQuestion} 
-                    setScore={setScore} 
-                    setTimer={setTimer} 
-                    setShowScore={setShowScore}
-                    />
+            <Option
+              currentQuestion={currentQuestion}
+              setCurrentQuestion={setCurrentQuestion}
+              setScore={setScore}
+              setTimer={setTimer}
+              setShowScore={setShowScore}
+            />
             <Timer timer={timer} />
           </>
-        }
-        
-        
+        )}
       </div>
     </main>
   );
---
+++ Quiz-app/src/container/score-section/Score.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ButtonRestart from "../../components/button/ButtonRestart";
+import QuestionsData from "../../questions.json";
+
+const Score = ({
+  score,
+  setCurrentQuestion,
+  setScore,
+  setShowScore,
+  setTimer,
+}) => {
+  return (
+    <div className="score-section">
+      <h2 className="capitalize my-3 text-xl font-medium mb-4">
+        Your score : {score}/{QuestionsData.length}
+      </h2>
+      <ButtonRestart
+        onClick={() => {
+          setCurrentQuestion(0);
+          setScore(0);
+          setShowScore(false);
+          setTimer(15);
+        }}
+      />
+    </div>
+  );
+};
+
+export default Score;
